884e4ce3292212c0f99169aa29758d1fb3e701f6
Retr0A <102794452+Retr0Alt@users.noreply.github.com>
2022-06-22 14:21:35 +0300

    Added More Things

--- Assets/Scripts/GraphicsObject.cs
+++ Assets/Scripts/GraphicsObject.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Graphics Object", menuName = "New Graphics Object")]
+[CreateAssetMenu(fileName = "New Graphics Object", menuName = "Graphics Object")]
 public class GraphicsObject : ScriptableObject
 {
+    [Header("Effects")]
     public bool shouldUsePostProcess;
+    [Header("Shadows")]
     public bool shouldUseShadows;
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,6 +8,9 @@ public class Tile : MonoBehaviour
     public LayerMask defaultLayer;
     public LayerMask groundLayer;
 
+    [Header("Style")]
+    public Color defaultColor;
+
     [HideInInspector]
     public bool isGround { get; set; }
     [HideInInspector]
@@ -16,6 +19,8 @@ public class Tile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        defaultColor = gameObject.GetComponent<MeshRenderer>().material.color;
+
         if (isGround)
         {
             gameObject.layer = groundLayer;
@@ -24,6 +29,19 @@ public class Tile : MonoBehaviour
         {
             gameObject.layer = defaultLayer;
         }
+    }
+
+    private void OnMouseOver()
+    {
+        if (type == TileType.Ground)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = defaultColor - new Color(0.2f, 0.2f, 0.2f, 1.0f);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = defaultColor;
     }
 
     // Update is called once per frame
